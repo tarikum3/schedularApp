e6cc70dea295e8d39e178639603f11ea4b3f4527
tariku <tarikum3@gmail.com>
2024-11-23 08:20:56 -0800

    site name env

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,15 +7,15 @@ import Layout from "@/app/components/common/Layout";
 import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
-const { SITE_NAME } = process.env;
+const { NEXT_PUBLIC_NEXT_PUBLIC_SITE_NAME } = process.env;
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `http://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : "http://localhost:3000";
 export const metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: SITE_NAME!,
-    template: `%s | ${SITE_NAME}`,
+    default: NEXT_PUBLIC_NEXT_PUBLIC_SITE_NAME!,
+    template: `%s | ${NEXT_PUBLIC_NEXT_PUBLIC_SITE_NAME}`,
   },
   robots: {
     follow: true,
