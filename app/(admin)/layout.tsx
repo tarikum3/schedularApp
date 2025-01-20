ee7a9d1380cc5e397aa722e576bde856295a295e
tariku <tarikum3@gmail.com>
2025-01-20 04:16:19 -0800

    store provider

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,14 +1,18 @@
 
 import Header from '@components/admin/layout/Header';
+import StoreProvider from '@components/admin/storeProvider';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
+      <StoreProvider>
       <Header/>
     <div
     id="main-content"
     className=" flex flex-col relative overflow-y-auto pt-[80px] lg:ml-[270px]"
   >
     {children}
-  </div></div>
+  </div>
+  </StoreProvider>
+  </div>
   );
 }
