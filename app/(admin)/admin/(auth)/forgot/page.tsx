c08a1346f4e1ae2775da388c6c4c82daca405569
tariku <tarikum3@gmail.com>
2025-03-01 07:18:21 -0800

    authpage

---
+++ app/(admin)/admin/(auth)/forgot/page.tsx
@@ -0,0 +1,11 @@
+import ForgotPassword from "@/app/components/admin/components/auth/ForgotPassword";
+
+export default function ForgotPasswordPage() {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col ">
+        <ForgotPassword />
+      </div>
+    </main>
+  );
+}
