1c79243423515fd0220ed4b82e16321817a833bf
tariku <tarikum3@gmail.com>
2025-03-15 09:19:45 -0800

    deploy error4

--- lib/admin/store/services/day.service.ts
+++ lib/admin/store/services/day.service.ts
@@ -1,14 +1,15 @@
 import { serviceApi } from "@/lib/admin/store/services/serviceApi";
-import { DayPayload, Day } from "@/lib/admin/types/day.type";
+import { Prisma } from "@prisma/client";
 
 export interface Pagination {
   limit: number;
   page: number;
 }
-
+export type Day = Prisma.DayGetPayload<{}>;
+export type DayPayload = Prisma.DayCreateInput;
 export const DayApi = serviceApi.injectEndpoints({
   endpoints: (builder) => ({
-    getDayByYear: builder.query<Day, string>({
+    getDayByYear: builder.query<Day[], string>({
       query: (year) => {
         const params = new URLSearchParams({
           year: year.toString(),
@@ -19,6 +20,7 @@ export const DayApi = serviceApi.injectEndpoints({
           method: "GET",
         };
       },
+
       transformResponse: (response: { data: { days: Day[] } }): Day[] => {
         if (response?.data?.days) {
           return response.data.days;
