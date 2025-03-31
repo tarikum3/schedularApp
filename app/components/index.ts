eeac18fc7353db2dd993e9bea3cb41177f0e8c42
tariku <tarikum3@gmail.com>
2025-03-31 01:41:23 -0800

    clear files

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,7 +1,5 @@
-export { default as Logo } from "./Logo";
-export { default as Button } from "./Button";
+export { default as Logo } from "./icons/Logo";
+export { default as Button } from "./schedular/components/ui/Button";
 
-export { default as Input } from "./Input";
-export { default as ErrorMessage } from "./ErrorMessage";
-
-export { useUI } from "./context";
+export { default as Input } from "./schedular/components/ui/Input";
+export { default as ErrorMessage } from "./schedular/components/ui/ErrorMessage";
