cb4f067015bc5d10fc25771ec58cf96902e38111
tariku <tarikum3@gmail.com>
2025-05-29 11:54:28 -0800

    test cronenvm

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -4,6 +4,10 @@ import { sendScheduleReminderEmail } from "@/lib/email"; // Adjust the import pa
 import prisma from "@lib/prisma";
 
 export async function GET(request: NextRequest) {
+  await sendScheduleReminderEmail(
+    ["bishawm3@gmail.com"],
+    `Bearer ${process.env.CRON_SECRET}`
+  );
   const authHeader = request.headers.get("authorization");
   await sendScheduleReminderEmail(
     ["bishawm3@gmail.com"],
