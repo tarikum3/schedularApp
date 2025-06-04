112a58649b68b2c4a51b9c88d6b1e22f67ec29ca
tariku <tarikum3@gmail.com>
2025-06-04 13:20:33 -0800

    calendar skeleton

--- app/(schedular)/layout.tsx
+++ app/(schedular)/layout.tsx
@@ -1,15 +1,3 @@
-// import Header from "@/app/components/schedular/components/layout/Header";
-// export default function Layout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <>
-//       <div className="flex h-screen flex-col ">
-//         <Header />
-//         <div className="grow overflow-y-auto ">{children}</div>
-//       </div>
-//     </>
-//   );
-// }
-
 import LayoutWrapper from "@/app/components/schedular/components/layout/Layout";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
